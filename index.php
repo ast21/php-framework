98e3e467a28c7ee17a2589f8111ef9a7ff12b0cc
Anastas Mironov <ast@l80.ru>
2022-10-19 02:16:57 +0600

    feat: add views and routes

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 declare(strict_types=1);
 
-echo "Own PHP Framework\n";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$routes = include('./routes.php');
+
+foreach($routes as $routePath => $routeView) {
+	if ($routePath == $path) {
+		include("./views/$routeView.php");
+		exit();
+	}
+
+	include("./views/404.php");
+	exit();
+}
